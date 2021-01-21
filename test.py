b8f89ecf7627453b157156dabace3a22102eeb40
mariadelmar <mariadelmarbibiloni@hotmail.com>
2021-01-21 14:07:00 +0100

    typo in penalty name

--- test.py
+++ test.py
@@ -5,11 +5,11 @@ from cruiseshiptourism.tourist import Tourist
 
 
 t0 = ("barco", 39.568276, 2.637640, 0, tsk.cruise_utility)
-t1 = ("catedral", 39.567425, 2.648299, 0.9, tsk.low_penalitation)
-t2 = ("castell", 39.563814,	2.619354, 0.9, tsk.low_penalitation)
-t3 = ("almudaina", 39.567886, 2.647022, 0.6, tsk.standard_penalitation)
-t4 = ("tren de soller", 39.576665, 2.653782, 0.5, tsk.high_penalitation)
-t5 = ("Plaza mayor", 39.571371, 2.651813, 0.35, tsk.standard_penalitation)
+t1 = ("catedral", 39.567425, 2.648299, 0.9, tsk.low_penalty)
+t2 = ("castell", 39.563814,	2.619354, 0.9, tsk.low_penalty)
+t3 = ("almudaina", 39.567886, 2.647022, 0.6, tsk.standard_penalty)
+t4 = ("tren de soller", 39.576665, 2.653782, 0.5, tsk.high_penalty)
+t5 = ("Plaza mayor", 39.571371, 2.651813, 0.35, tsk.standard_penalty)
 
 tasks_ls = [t0, t1, t2, t3, t4, t5]
 tasks = pd\
