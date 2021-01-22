016d7b1286e7ed0087bb9b621a283cebee63d2bc
mariadelmar <mariadelmarbibiloni@hotmail.com>
2021-01-22 17:40:03 +0100

    ship utility as 1 if  time > 10 and task-1=ship

--- tourist.py
+++ tourist.py
@@ -8,7 +8,7 @@ class Tourist:
         self.task_route = [None] * (visit_time + 1)
         self.task_list = [None] * (visit_time + 1)
 
-    def tourist_route(self):
+    def tourist_route(self, tship=15):
         time = 0
         i = 0  # cruise ship must be task 0
         tasks = self.initial_tasks.copy()
@@ -26,5 +26,8 @@ class Tourist:
             self.task_route[time] = i
             if i:
                 tasks.loc[[i], ["utility"]] = tasks.update_utility[i](tasks.utility[i])
+            elif time == tship or (time > 10 and self.task_route[time-1] == 0):
+                # If the tourist goes to the ship in time >= timeship, the tourist should stay in the ship
+                tasks.loc[[0], ["utility"]] = 1
             tasks.loc[[0], ["utility"]] = tsk.cruise_utility(time, tasks.utility, breaks=(8, 10))
             self.task_list[time] = tasks.iloc[:, 0:-1].copy()
