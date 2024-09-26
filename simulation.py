b048a87602e059173fe863ab651e10dc72fa333b
MBibiloni <mariadelmarbibiloni@hotmail.com>
2024-09-26 15:50:07 +0200

    add iterations

--- simulation.py
+++ simulation.py
@@ -82,14 +82,20 @@ def simulation(df_tasks, ntourists, aggregation_function, decision_method, time=
 
     return {"tourist_routes": tourist_routes, "summary": summary_df}
 
+def dataframe_mean(df_list):
+    n = len(df_list)
+    df = df_list[0]
+    for i in range(1, n):
+        df = df.add(df_list[i])
+    return (1/n)*df
 
 if __name__ == "__main__":
 
-    ntourists, time, aggregation_function, decision_method, noise_sigma, owa_weight = sa.get_sysarg()
-    if not (ntourists or time or aggregation_function or decision_method or noise_sigma or owa_weight):
+    ntourists, time, aggregation_function, decision_method, noise_sigma, owa_weight, niterations = sa.get_sysarg()
+    if not (ntourists or time or aggregation_function or decision_method or noise_sigma or owa_weight or niterations):
         message = """
         You must introduce all the parameters:
-            simulation.py -n <ntourists> -t <time> -a <aggregation_function> -d <decision_method> -s <noise_sigma> -w <owa_weight>
+            simulation.py -n <ntourists> -t <time> -a <aggregation_function> -d <decision_method> -s <noise_sigma> -w <owa_weight> -i <niterations>
         """
         raise Exception(message)
 
@@ -109,22 +115,34 @@ if __name__ == "__main__":
     tasks = add_utility(tasks)
     tasks = add_num_tourists(tasks, ntourists)
     
-    sim_results = simulation(tasks, int(ntourists), aggregation_function, decision_method, time=int(time),
-         u_noise_sigma=float(noise_sigma), owa_weight=ast.literal_eval(owa_weight))
+    niterations = int(niterations)
+    sim_summaries = [None] * niterations
+    for i in range(0, niterations):
+        sim_results = simulation(tasks, int(ntourists), aggregation_function, decision_method, time=int(time),
+            u_noise_sigma=float(noise_sigma), owa_weight=ast.literal_eval(owa_weight))
+        
+        sim_summaries[i] = sim_results["summary"]
 
+        #1st simulation result
+        if i == 0:
+            if not owa_weight:
+                sim_results["tourist_routes"].to_csv(
+                    f'test_sim/palma_poi_troutes_{ntourists}_{time}_{aggregation_function}_{decision_method}_noise_sigma_{noise_sigma}_it-{niterations}.csv',
+                    index=False)
+            else:
+                sim_results["tourist_routes"].to_csv(
+                    f'test_sim/palma_poi_troutes_{ntourists}_{time}_{aggregation_function}_{decision_method}_noise_sigma_{noise_sigma}_{owa_weight}_it-{niterations}.csv',
+                    index=False)
+
+    # mean of all simulations
+    sim_summary=dataframe_mean(sim_summaries)
     if not owa_weight:
-        sim_results["tourist_routes"].to_csv(
-            f'test_sim/palma_poi_troutes_{ntourists}_{time}_{aggregation_function}_{decision_method}_noise_sigma_{noise_sigma}.csv',
-            index=False)
-        sim_results["summary"].to_csv(
-            f'test_sim/palma_poi_summary_{ntourists}_{time}_{aggregation_function}_{decision_method}_noise_sigma_{noise_sigma}.csv',
+        sim_summary.to_csv(
+            f'test_sim/palma_poi_summary_{ntourists}_{time}_{aggregation_function}_{decision_method}_noise_sigma_{noise_sigma}_it-{niterations}.csv',
             index=False)
     else:
-        sim_results["tourist_routes"].to_csv(
-            f'test_sim/palma_poi_troutes_{ntourists}_{time}_{aggregation_function}_{decision_method}_noise_sigma_{noise_sigma}_{owa_weight}.csv',
-            index=False)
-        sim_results["summary"].to_csv(
-            f'test_sim/palma_poi_summary_{ntourists}_{time}_{aggregation_function}_{decision_method}_noise_sigma_{noise_sigma}_{owa_weight}.csv',
+        sim_summary.to_csv(
+            f'test_sim/palma_poi_summary_{ntourists}_{time}_{aggregation_function}_{decision_method}_noise_sigma_{noise_sigma}_{owa_weight}_it-{niterations}.csv',
             index=False)
 
 executionTime = (t.time() - startTime)
--- sys_arguments.py
+++ sys_arguments.py
@@ -6,19 +6,20 @@ def get_sysarg():
     try:
         options, remainder = getopt.getopt(
             sys.argv[1:],
-            'n:t:a:d:i:m:w:',
+            'n:t:a:d:i:s:w:i:',
             ['ntourists=',
              'time=',
              'aggregation_function=',
              'decision_method=',
-             'noise_sigma='
-             'owa_weight='
+             'noise_sigma=',
+             'owa_weight=',
+             'niterations=',
              ])
     except getopt.GetoptError as err:
         print('ERROR:', err)
         sys.exit(1)
 
-    ntourists, time, aggregation_function, decision_method, noise_sigma, owa_weight = [None for i in range(6)]
+    ntourists, time, aggregation_function, decision_method, noise_sigma, owa_weight, niterations = [None for i in range(7)]
     for opt, arg in options:
         if opt in ('-n', '--ntourists'):
             ntourists = arg
@@ -28,9 +29,11 @@ def get_sysarg():
             aggregation_function = arg
         elif opt in ('-d', '--decision_method'):
             decision_method = arg
-        elif opt in ('-m', '--noise_sigma'):
+        elif opt in ('-s', '--noise_sigma'):
             noise_sigma = arg
         elif opt in ('-w', '--owa_weight'):
             owa_weight = arg
+        elif opt in ('-i', '--niterations'):
+            niterations = arg
 
-    return ntourists, time, aggregation_function, decision_method, noise_sigma, owa_weight
+    return ntourists, time, aggregation_function, decision_method, noise_sigma, owa_weight, niterations
